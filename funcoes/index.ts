862ee1f213475f55a1d3ea3862bd69d87ce65ef8
Luciel Santos <luciel.vitor.bs@gmail.com>
2024-05-23 20:48:21 -0300

    adicionado exemplos de função

--- funcoes/index.ts
+++ funcoes/index.ts
@@ -1,3 +1,63 @@
-console.log("Hello world");
-console.log("Hello world numero 2");
-console.log("Hello world numero 3");
+interface User {
+  name: string;
+  surname?: string;
+  status: boolean;
+  yearsOld: number;
+}
+
+const user: User = {
+  name: "Wagner",
+  yearsOld: 3,
+  status: false,
+};
+
+function formatName(userToFormat: User): string | number {
+  if (userToFormat.surname) {
+    return `${userToFormat.name} ${userToFormat.surname} tem ${userToFormat.yearsOld} anos`;
+  }
+
+  if (!userToFormat.status) {
+    return userToFormat.yearsOld;
+  }
+
+  return `${userToFormat.name} tem ${userToFormat.yearsOld} anos`;
+}
+
+// console.log(formatName(user));
+
+function formatDate(dateParam?: Date, addYear?: boolean): string {
+  const date = dateParam || new Date();
+
+  const day = date.getDate();
+  /**
+   * 0 - Janeiro
+   * 1 - Fevereiro
+   * ...
+   */
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  if (addYear) {
+    return `${day}/${month}/${year}`;
+  }
+
+  return `${day}/${month}`;
+}
+
+console.log(formatDate(new Date(1999, 5, 4), true));
+console.log(formatDate(new Date(1999, 5, 4)));
+console.log(formatDate());
+
+// function compara(param?: any) {
+//   // if (param) {
+//   //   return param;
+//   // } else {
+//   //   return "Bloco falso";
+//   // }
+
+//   // return param ? param : "Bloco falso";
+
+//   return param || "Bloco falso";
+// }
+
+// console.log(compara());
